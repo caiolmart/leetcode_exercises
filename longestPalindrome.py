19ca00359ac6cb327dcccdc541a95aa9b09fe4a9
Caio Martinelli <caio.martinelli@hotmail.com>
2025-05-08 18:27:50 -0300

    Use dynamic programing

--- longestPalindrome.py
+++ longestPalindrome.py
@@ -1,45 +1,39 @@
 class Solution:
-    def _is_palindrome(self, start, end, s):
-        offset = 0
-        while offset < (start - end) // 2:
-            if s[start + offset] != s[end - offset]:
-                return False
-            offset += 1
-        return True
-
-    def longest_for_offset(self, s, s_inv, offset):
-        if offset >= 0:
-            this_s = s[offset:]
-            this_s_inv = s_inv[:-offset] or s_inv # goes null for offset=0
-        else:
-            this_s = s[:offset]
-            this_s_inv = s_inv[-offset:]
-        comp_list = [a == b for a, b in  zip(this_s, this_s_inv)]
-        if all(comp_list):
-            return this_s
-
-        for remove_n in range(1, len(comp_list)//2 + len(comp_list)%2):
-            if all(comp_list[remove_n: -remove_n]):
-                return this_s[remove_n: -remove_n]
-        return ''
-
     def longestPalindrome(self, s: str) -> str:
-        s_inv = s[::-1]
-        longest = ''
-        for offset in range(-len(s) + 1, len(s)):
-            this_longest = self.longest_for_offset(s, s_inv, offset)
-            if len(this_longest) > len(longest):
-                longest = this_longest
-            
+        is_palindrome_matrix = [
+            [i == j for i in range(len(s))] for j in range(len(s))
+        ]
+        best_pos = (0, 0)
 
-        return longest
+        # parallel diagonal
+        for i in range(len(s) - 1):
+            if s[i] == s[i + 1]:
+                is_palindrome_matrix[i][i + 1] = True
+                best_pos = (i, i + 1)
+
+        for offset in range(2, len(s)):
+            for i in range(0, len(s) - offset):
+                if (
+                    is_palindrome_matrix[i + 1][i + offset - 1]
+                    and s[i] == s[i + offset]
+                ):
+                    is_palindrome_matrix[i][i + offset] = True
+                    best_pos = (i, i + offset)
+
+        return s[best_pos[0] : best_pos[1] + 1]
 
 
 sol = Solution()
-print(sol.longestPalindrome('cbbd'))
+print(sol.longestPalindrome("cbbd"))
 print(sol.longestPalindrome("ccd"))
-print(sol.longestPalindrome('aacabdkacaa'))
-print(sol.longestPalindrome("dtgrtoxuybwyfskikukcqlvprfipgaygawcqnfhpxoifwgpnzjfdnhpgmsoqzlpsaxmeswlhzdxoxobxysgmpkhcylvqlzenzhzhnakctrliyyngrquiuvhpcrnccapuuwrrdufwwungaevzkvwbkcietiqsxpvaaowrteqgkvovcoqumgrlsxvouaqzwaylehybqchsgpzbkfugujrostopwhtgrnrggocprnxwsecmvofawkkpjvcchtxixjtrnngrzqpiwywmnbdnjwqpmnifdiwzpmabosrmzhgdwgcgidmubywsnshcgcrawjvfiuxzyzxsbpfhzpfkjqcpfyynlpshzqectcnltfimkukopjzzmlfkwlgbzftsddnxrjootpdhjehaafudkkffmcnimnfzzjjlggzvqozcumjyazchjkspdlmifvsciqzgcbehqvrwjkusapzzxyiwxlcwpzvdsyqcfnguoidiiekwcjdvbxjvgwgcjcmjwbizhhcgirebhsplioytrgjqwrpwdciaeizxssedsylptffwhnedriqozvfcnsmxmdxdtflwjvrvmyausnzlrgcchmyrgvazjqmvttabnhffoe"))
-print(sol.longestPalindrome("pihoigwlvzvtrugdolvtzrkyelaqdvbijzmkhebzawboaxkdjyfocpewwztffuaibcqurwwmijmvrnpfcoglyxpxkrbhupoxcafabxtoecodsjgngrionuvzaiigevuvruxxiwpjzjlqgenglhprcgzgpdzabrjhkbtfrbmwpcszepxhwiwdhvnpsmhhaiqsbeiwsaeomqtzcpjzfknejxlxwtpkufanhuoyjgihdzhtxnyctazzvnttjspfztjurdwmmzrvobcatkorfhpieoqfetcglembkgbexsznuduhrfoxkbswkanqwfkoktnnujqetijaqhrxuhkgsezfdrncbaltctwcourdbpdwhqlsxfwsoaduaqkbjeekwwykptjthhtokrvzsuelsywyznqscnwiszogzqvfsgggzltlvzkllinpfaigswquqfvabbzvestwxhbnfjhnvfhyxalchmtkcwnyyrbwjsoqooypwteozbivqiyldpqlykxinmzkgnmfbobgjivlzubfen"))
-
-
+print(sol.longestPalindrome("aacabdkacaa"))
+print(
+    sol.longestPalindrome(
+        "dtgrtoxuybwyfskikukcqlvprfipgaygawcqnfhpxoifwgpnzjfdnhpgmsoqzlpsaxmeswlhzdxoxobxysgmpkhcylvqlzenzhzhnakctrliyyngrquiuvhpcrnccapuuwrrdufwwungaevzkvwbkcietiqsxpvaaowrteqgkvovcoqumgrlsxvouaqzwaylehybqchsgpzbkfugujrostopwhtgrnrggocprnxwsecmvofawkkpjvcchtxixjtrnngrzqpiwywmnbdnjwqpmnifdiwzpmabosrmzhgdwgcgidmubywsnshcgcrawjvfiuxzyzxsbpfhzpfkjqcpfyynlpshzqectcnltfimkukopjzzmlfkwlgbzftsddnxrjootpdhjehaafudkkffmcnimnfzzjjlggzvqozcumjyazchjkspdlmifvsciqzgcbehqvrwjkusapzzxyiwxlcwpzvdsyqcfnguoidiiekwcjdvbxjvgwgcjcmjwbizhhcgirebhsplioytrgjqwrpwdciaeizxssedsylptffwhnedriqozvfcnsmxmdxdtflwjvrvmyausnzlrgcchmyrgvazjqmvttabnhffoe"
+    )
+)
+print(
+    sol.longestPalindrome(
+        "pihoigwlvzvtrugdolvtzrkyelaqdvbijzmkhebzawboaxkdjyfocpewwztffuaibcqurwwmijmvrnpfcoglyxpxkrbhupoxcafabxtoecodsjgngrionuvzaiigevuvruxxiwpjzjlqgenglhprcgzgpdzabrjhkbtfrbmwpcszepxhwiwdhvnpsmhhaiqsbeiwsaeomqtzcpjzfknejxlxwtpkufanhuoyjgihdzhtxnyctazzvnttjspfztjurdwmmzrvobcatkorfhpieoqfetcglembkgbexsznuduhrfoxkbswkanqwfkoktnnujqetijaqhrxuhkgsezfdrncbaltctwcourdbpdwhqlsxfwsoaduaqkbjeekwwykptjthhtokrvzsuelsywyznqscnwiszogzqvfsgggzltlvzkllinpfaigswquqfvabbzvestwxhbnfjhnvfhyxalchmtkcwnyyrbwjsoqooypwteozbivqiyldpqlykxinmzkgnmfbobgjivlzubfen"
+    )
+)
